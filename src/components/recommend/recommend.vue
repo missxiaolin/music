d5d021507a554e0e54dab6bad0d4cc1628d544d0
MAC <surprisepeas@gmail.com>
2017-09-16 19:57:42 +0800

    * 使用better-scroll 加入滚动

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,36 +1,41 @@
 <template>
   <div class="recommend" ref="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length > 0" class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommends">
+    <Scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length > 0" class="slider-wrapper">
+          <slider>
+            <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="">
               </a>
-          </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="item in discList" class="item">
-            <div class="icon">
-              <img width="60" height="60" :src="item.imgurl" alt="">
             </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </Scroll>
 
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+// 轮播图组件
 import Slider from 'base/slider/slider'
+// 滚动组件
+import Scroll from 'base/scroll/scroll'
 import { getRecommend, getDiscList } from 'api/recommend'
 
 import { ERR_OK } from 'api/config'
@@ -39,7 +44,7 @@ export default {
   data() {
     return {
       recommends: [], // 推荐数据
-      discList: []
+      discList: [], // 列表
     }
   },
   created() {
@@ -56,16 +61,24 @@ export default {
       })
     },
     // 获取歌单
-    _getDiscList: function(){
+    _getDiscList() {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
           this.discList = res.data.list
         }
       })
+    },
+    // 监听图片
+    loadImage() {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        this.$refs.scroll.refresh()
+      }
     }
   },
   components: {
     Slider,
+    Scroll
   }
 }
 </script>
