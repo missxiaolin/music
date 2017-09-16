7fcf038255d885da69c0e2e5f6a3a8e9e22a5c40
MAC <surprisepeas@gmail.com>
2017-09-16 15:15:14 +0800

    * 增加移动端点击300毫秒延迟

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
+// 解决移动端300毫秒点击延迟
+import fastclick from 'fastclick'
+
 import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+
+
 
 /* eslint-disable no-new */
 new Vue({
