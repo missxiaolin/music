2975f82945694cabbd88fb2529411da8db15717f
MAC <surprisepeas@gmail.com>
2017-09-16 16:50:34 +0800

    * 基本架构样式

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <div id="app">
-    <MHeader></MHeader>
+  <div id="app" @touchmove.prevent>
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    <player></player>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import MHeader from 'components/m-header/m-header'
-export default {
-  name: 'app',
-  components: {
-    MHeader
+  import MHeader from 'components/m-header/m-header'
+  import Player from 'components/player/player'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab,
+      Player
+    }
   }
-}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-    @import "common/stylus/variable.styl";
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import recommend from '@/components/recommend/recommend'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'recommend',
+      component: recommend
     }
   ]
 })
