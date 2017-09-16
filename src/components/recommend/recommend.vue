d5817da5b433ba78d5e5ba0ac5cc1f6abbe5b758
MAC <surprisepeas@gmail.com>
2017-09-16 20:05:52 +0800

    * 加入图片懒加载

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl" alt="">
+                <img width="60" height="60" v-lazy="item.imgurl" alt="">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -14,6 +15,10 @@ import 'common/stylus/index.styl'
 // import vConsole from 'vconsole'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 
 // fastclick.attach(document.body)
 /* eslint-disable no-new */
