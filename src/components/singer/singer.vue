18d2003d2389c6fdf19f6b43e1f99e869bbc00ef
MAC <surprisepeas@gmail.com>
2017-09-20 12:54:42 +0800

    * 初始化歌手页面

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="singer" ref="singer">
+    歌手
+  </div>
+</template>
+
+<script>
+import { getSingerList } from 'api/singer'
+import Singer from 'common/js/singer'
+import { ERR_OK } from 'api/config'
+
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    // 歌手
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+          console.log(this.singers)
+        }
+      })
+    },
+    // 格式化数组
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    },
+  }
+}
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// 首页
 import recommend from '@/components/recommend/recommend'
+// 歌手
+import singer from '@/components/singer/singer'
 
 Vue.use(Router)
 
@@ -11,6 +14,11 @@ export default new Router({
       path: '/recommend',
       name: 'recommend',
       component: recommend
+    },
+    {
+      path: '/singer',
+      name: 'singer',
+      component: singer
     }
   ]
 })
