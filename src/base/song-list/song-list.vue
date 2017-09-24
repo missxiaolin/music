91ea22621cacadc14d71012fb9806c114d3d4070
MAC <surprisepeas@gmail.com>
2017-09-24 20:34:41 +0800

    * 增加基础组件song-list

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      songs: {
+        type: Array,
+        default: []
+      },
+      rank: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .song-list
+    .item
+      display: flex
+      align-items: center
+      box-sizing: border-box
+      height: 64px
+      font-size: $font-size-medium
+      .rank
+        flex: 0 0 25px
+        width: 25px
+        margin-right: 30px
+        text-align: center
+        .icon
+          display: inline-block
+          width: 25px
+          height: 24px
+          background-size: 25px 24px
+          &.icon0
+            bg-image('first')
+          &.icon1
+            bg-image('second')
+          &.icon2
+            bg-image('third')
+        .text
+          color: $color-theme
+          font-size: $font-size-large
+      .content
+        flex: 1
+        line-height: 20px
+        overflow: hidden
+        .name
+          no-wrap()
+          color: $color-text
+        .desc
+          no-wrap()
+          margin-top: 4px
+          color: $color-text-d
+</style>
