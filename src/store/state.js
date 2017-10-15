9d0a0ea1049960764bd7ee6fdcb0f53f61bbb90b
MAC <surprisepeas@gmail.com>
2017-10-15 15:16:49 +0800

    * VUEX 增加一些状态管理

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playlist: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1 // 当前播放索引
 }
 
 export default state
