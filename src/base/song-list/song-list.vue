1df6e7ad47e1ef500228639ce6089eaf56812665
MAC <surprisepeas@gmail.com>
2017-10-15 17:11:45 +0800

    * 组件雏形

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="song-list">
     <ul>
-      <li v-for="song in songs" class="item">
+      <li v-for="(song,index) in songs" class="item" @click="selectItem(song,index)">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -12,65 +12,89 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      songs: {
-        type: Array,
-        default: []
-      },
-      rank: {
-        type: Boolean,
-        default: false
-      }
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: []
     },
-    methods: {
-      // desc
-      getDesc(song) {
-        return `${song.singer}·${song.album}`
-      },
+    rank: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    // desc
+    getDesc(song) {
+      return `${song.singer}·${song.album}`
+    },
+    // 点击歌曲
+    selectItem(item, index) {
+      this.$emit('select', item, index)
     }
   }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
-  @import "~common/stylus/mixin"
+@import '~common/stylus/variable';
+@import '~common/stylus/mixin';
 
-  .song-list
-    .item
-      display: flex
-      align-items: center
-      box-sizing: border-box
-      height: 64px
-      font-size: $font-size-medium
-      .rank
-        flex: 0 0 25px
-        width: 25px
-        margin-right: 30px
-        text-align: center
-        .icon
-          display: inline-block
-          width: 25px
-          height: 24px
-          background-size: 25px 24px
-          &.icon0
-            bg-image('first')
-          &.icon1
-            bg-image('second')
-          &.icon2
-            bg-image('third')
-        .text
-          color: $color-theme
-          font-size: $font-size-large
-      .content
-        flex: 1
-        line-height: 20px
-        overflow: hidden
-        .name
-          no-wrap()
-          color: $color-text
-        .desc
-          no-wrap()
-          margin-top: 4px
-          color: $color-text-d
+.song-list {
+  .item {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    height: 64px;
+    font-size: $font-size-medium;
+
+    .rank {
+      flex: 0 0 25px;
+      width: 25px;
+      margin-right: 30px;
+      text-align: center;
+
+      .icon {
+        display: inline-block;
+        width: 25px;
+        height: 24px;
+        background-size: 25px 24px;
+
+        &.icon0 {
+          bg-image('first');
+        }
+
+        &.icon1 {
+          bg-image('second');
+        }
+
+        &.icon2 {
+          bg-image('third');
+        }
+      }
+
+      .text {
+        color: $color-theme;
+        font-size: $font-size-large;
+      }
+    }
+
+    .content {
+      flex: 1;
+      line-height: 20px;
+      overflow: hidden;
+
+      .name {
+        no-wrap();
+        color: $color-text;
+      }
+
+      .desc {
+        no-wrap();
+        margin-top: 4px;
+        color: $color-text-d;
+      }
+    }
+  }
+}
 </style>
