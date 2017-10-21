1067d708148e3261c59770cc0ce5859a6f99b5cc
xiaolin <462441355@qq.com>
2017-10-21 13:04:20 +0800

    * 初始化歌单页面

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import recommend from '@/components/recommend/recommend'
 import singer from '@/components/singer/singer'
 // 歌手详情
 import singerDetail from '@/components/singer-detail/singer-detail'
+// 歌单页面
+import disc from '@/components/disc/disc'
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/recommend',
       name: 'recommend',
       component: recommend
+    }, {
+      path: '/recommend',
+      component: recommend,
+      children: [
+        {
+          path: ':id',
+          name: 'disc',
+          component: disc
+        }
+      ]
     }, {
       path: '/singer',
       name: 'singer',
