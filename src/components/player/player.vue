fef683c2d808cca4be757ad053a642c06a57215e
xiaolin <462441355@qq.com>
2017-11-12 11:29:16 +0800

    * 添加play list组件

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -89,6 +89,7 @@
         </div>
       </div>
     </transition>
+    <playlist ref="playlist"></playlist>
     <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
   </div>
 </template>
@@ -103,6 +104,7 @@ import { playMode } from "common/js/config";
 import { shuffle } from "common/js/util";
 import Lyric from "lyric-parser";
 import Scroll from "base/scroll/scroll";
+import Playlist from 'components/playlist/playlist';
 
 const transform = prefixStyle("transform");
 const transitionDuration = prefixStyle("transitionDuration");
@@ -495,7 +497,8 @@ export default {
   components: {
     ProgressBar,
     ProgressCircle,
-    Scroll
+    Scroll,
+    Playlist
   }
 };
 </script>
---
+++ src/components/playlist/playlist.vue
@@ -0,0 +1,232 @@
+<template>
+  <transition name="list-fade">
+    <div class="playlist" @click="hide" v-show="showFlag">
+      <div class="list-wrapper" @click.stop>
+        <div class="list-header">
+          <h1 class="title">
+            <i class="icon" :class="iconMode" @click="changeMode"></i>
+            <span class="text">{{modeText}}</span>
+            <span class="clear" @click="showConfirm"><i class="icon-clear"></i></span>
+          </h1>
+        </div>
+        <scroll ref="listContent" :data="sequenceList" class="list-content" :refreshDelay="refreshDelay">
+          <transition-group ref="list" name="list" tag="ul">
+            <li :key="item.id" class="item" v-for="(item,index) in sequenceList"
+                @click="selectItem(item,index)">
+              <i class="current" :class="getCurrentIcon(item)"></i>
+              <span class="text">{{item.name}}</span>
+              <span @click.stop="toggleFavorite(item)" class="like">
+                <i :class="getFavoriteIcon(item)"></i>
+              </span>
+              <span @click.stop="deleteOne(item)" class="delete">
+                <i class="icon-delete"></i>
+              </span>
+            </li>
+          </transition-group>
+        </scroll>
+        <div class="list-operate">
+          <div @click="addSong" class="add">
+            <i class="icon-add"></i>
+            <span class="text">添加歌曲到队列</span>
+          </div>
+        </div>
+        <div @click="hide" class="list-close">
+          <span>关闭</span>
+        </div>
+      </div>
+      <confirm ref="confirm" @confirm="confirmClear" text="是否清空播放列表" confirmBtnText="清空"></confirm>
+    </div>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+import { mapActions } from "vuex";
+import Scroll from "base/scroll/scroll";
+import Confirm from "base/confirm/confirm";
+
+export default {
+  data() {
+    return {
+      showFlag: false,
+      refreshDelay: 120,
+      iconMode: '',
+      modeText: '',
+      sequenceList: []
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+    // 清空
+    confirmClear() {},
+    // 关闭
+    hide() {},
+    // 添加
+    addSong() {},
+    deleteOne() {},
+    toggleFavorite() {},
+    selectItem() {},
+    showConfirm() {},
+    changeMode(){},
+  },
+  watch: {},
+  components: {
+    Scroll,
+    Confirm
+  }
+};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+@import '~common/stylus/mixin';
+
+.playlist {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 200;
+  background-color: $color-background-d;
+
+  &.list-fade-enter-active, &.list-fade-leave-active {
+    transition: opacity 0.3s;
+
+    .list-wrapper {
+      transition: all 0.3s;
+    }
+  }
+
+  &.list-fade-enter, &.list-fade-leave-to {
+    opacity: 0;
+
+    .list-wrapper {
+      transform: translate3d(0, 100%, 0);
+    }
+  }
+
+  &.list-fade-enter, .list-wrapper {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: $color-highlight-background;
+
+    .list-header {
+      position: relative;
+      padding: 20px 30px 10px 20px;
+
+      .title {
+        display: flex;
+        align-items: center;
+
+        .icon {
+          margin-right: 10px;
+          font-size: 30px;
+          color: $color-theme-d;
+        }
+
+        .text {
+          flex: 1;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+        }
+
+        .clear {
+          extend-click();
+
+          .icon-clear {
+            font-size: $font-size-medium;
+            color: $color-text-d;
+          }
+        }
+      }
+    }
+
+    .list-content {
+      max-height: 240px;
+      overflow: hidden;
+
+      .item {
+        display: flex;
+        align-items: center;
+        height: 40px;
+        padding: 0 30px 0 20px;
+        overflow: hidden;
+
+        &.list-enter-active, &.list-leave-active {
+          transition: all 0.1s;
+        }
+
+        &.list-enter, &.list-leave-to {
+          height: 0;
+        }
+
+        .current {
+          flex: 0 0 20px;
+          width: 20px;
+          font-size: $font-size-small;
+          color: $color-theme-d;
+        }
+
+        .text {
+          flex: 1;
+          no-wrap();
+          font-size: $font-size-medium;
+          color: $color-text-d;
+        }
+
+        .like {
+          extend-click();
+          margin-right: 15px;
+          font-size: $font-size-small;
+          color: $color-theme;
+
+          .icon-favorite {
+            color: $color-sub-theme;
+          }
+        }
+
+        .delete {
+          extend-click();
+          font-size: $font-size-small;
+          color: $color-theme;
+        }
+      }
+    }
+
+    .list-operate {
+      width: 140px;
+      margin: 20px auto 30px auto;
+
+      .add {
+        display: flex;
+        align-items: center;
+        padding: 8px 16px;
+        border: 1px solid $color-text-l;
+        border-radius: 100px;
+        color: $color-text-l;
+
+        .icon-add {
+          margin-right: 5px;
+          font-size: $font-size-small-s;
+        }
+
+        .text {
+          font-size: $font-size-small;
+        }
+      }
+    }
+
+    .list-close {
+      text-align: center;
+      line-height: 50px;
+      background: $color-background;
+      font-size: $font-size-medium-x;
+      color: $color-text-l;
+    }
+  }
+}
+</style>
