bbb150eac544dc7de4456cba09922e46023ac3b4
xiaolin <462441355@qq.com>
2017-11-13 23:49:19 +0800

    * add

--- src/base/switches/switches.vue
+++ src/base/switches/switches.vue
@@ -8,42 +8,48 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      switches: {
-        type: Array,
-        default: []
-      },
-      currentIndex: {
-        type: Number,
-        default: 0
-      }
+export default {
+  props: {
+    switches: {
+      type: Array,
+      default: []
     },
-    methods: {
-      switchItem(index) {
-        this.$emit('switch', index)
-      }
+    currentIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    // 派发单击事件
+    switchItem(index) {
+      this.$emit("switch", index);
     }
   }
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .switches
-    display: flex
-    align-items: center
-    width: 240px
-    margin: 0 auto
-    border: 1px solid $color-highlight-background
-    border-radius: 5px
-    .switch-item
-      flex: 1
-      padding: 8px
-      text-align: center
-      font-size: $font-size-medium
-      color: $color-text-d
-      &.active
-        background: $color-highlight-background
-        color: $color-text
+.switches {
+  display: flex;
+  align-items: center;
+  width: 240px;
+  margin: 0 auto;
+  border: 1px solid $color-highlight-background;
+  border-radius: 5px;
+
+  .switch-item {
+    flex: 1;
+    padding: 8px;
+    text-align: center;
+    font-size: $font-size-medium;
+    color: $color-text-d;
+
+    &.active {
+      background: $color-highlight-background;
+      color: $color-text;
+    }
+  }
+}
 </style>
--- src/base/top-tip/top-tip.vue
+++ src/base/top-tip/top-tip.vue
@@ -7,44 +7,49 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      delay: {
-        type: Number,
-        default: 2000
-      }
+export default {
+  props: {
+    delay: {
+      type: Number,
+      default: 2000
+    }
+  },
+  data() {
+    return {
+      showFlag: false
+    };
+  },
+  methods: {
+    show() {
+      this.showFlag = true;
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.hide();
+      }, this.delay);
     },
-    data() {
-      return {
-        showFlag: false
-      }
-    },
-    methods: {
-      show() {
-        this.showFlag = true
-        clearTimeout(this.timer)
-        this.timer = setTimeout(() => {
-          this.hide()
-        }, this.delay)
-      },
-      hide() {
-        this.showFlag = false
-      }
+    hide() {
+      this.showFlag = false;
     }
   }
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .top-tip
-    position: fixed
-    top: 0
-    width: 100%
-    z-index: 500
-    background: $color-dialog-background
-    &.drop-enter-active, &.drop-leave-active
-      transition: all 0.3s
-    &.drop-enter, &.drop-leave-to
-      transform: translate3d(0, -100%, 0)
+.top-tip {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 500;
+  background: $color-dialog-background;
+
+  &.drop-enter-active, &.drop-leave-active {
+    transition: all 0.3s;
+  }
+
+  &.drop-enter, &.drop-leave-to {
+    transform: translate3d(0, -100%, 0);
+  }
+}
 </style>
